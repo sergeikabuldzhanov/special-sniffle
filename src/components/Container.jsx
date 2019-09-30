e13c30866d55f11cd343c86d952a055b95c4593b
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-09-30 08:06:09 +0200

    initial commit

---
+++ src/components/Container.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Container.less';
+
+
+export default function Container() {
+  return (
+    <div className='container-hello-world'>
+      Hello, World
+    </div>
+  );
+}
