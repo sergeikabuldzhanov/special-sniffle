423eefd72db7c53ffc08ee35872aa9f250dde93c
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-09-30 09:46:46 +0200

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import Todos from './components/Todos';
 import Counter from './components/Counter';
