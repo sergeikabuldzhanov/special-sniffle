c781342958cfe44eaab1e836d8e9e2685dd69608
sergeikabuldzhanov <thesiegebot@gmail.com>
2019-12-02 16:18:51 +0500

    go to form component, try to input something, uncomment line 48, try again

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,5 @@
 import React, { useState } from 'react';
 
-export default class {
-  // implement
-  // it's OK if you don't have time to change the methods
-}
-
 export function FormOld() {
   const [formValues, setFormValues] = useState({
     fname: '',
@@ -33,4 +28,52 @@ export function FormOld() {
       <button>submit</button>
     </form>
   );
+}
+
+export default class Form extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      formValues : {
+        fname: '',
+        lname: '',
+    }}
+    this.onValueChange = this.onValueChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  onValueChange (event){
+    console.log(event);
+    
+    // event.persist();    
+    this.setState(oldState =>({
+      formValues:{
+        ...oldState.formValues,
+        [event.target.name]: event.target.value,
+      }
+    }));
+  };
+
+  onFormSubmit (event){
+    event.preventDefault();
+    alert(`submitting ${this.state.formValues.lname}, ${this.state.formValues.fname}`);
+  };
+
+  render() {
+
+    return (
+      <form className='component' onSubmit={this.onFormSubmit}>
+        <label>first name
+          <input value={this.state.formValues.fname} onChange={this.onValueChange} name='fname' />
+        </label><br />
+  
+        <label>last name
+          <input value={this.state.formValues.lname} onChange={this.onValueChange} name='lname' />
+        </label><br />
+  
+        <button>submit</button>
+      </form>
+    );
+    
+  }
 }
